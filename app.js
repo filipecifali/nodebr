0a42a9435f016dfa3d16b65604ff0b0cf727964c
Alan Hoffmeister <alanhoffmeister@gmail.com>
2015-09-01 12:33:36 -0300

    Iniciando refatoramento para ES6

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 // Aqui é onde a magia acontece :-)
-var server = require('./lib/server');
-var config = require('config');
-var inst = null;
-var log = require('./lib/log');
+import server from './lib/server';
+import config from 'config';
+import log from './lib/log';
+
+let inst = null;
 
 log.info('Iniciando bootstrap do servidor HTTP');
 server.start()
-  .then(function(i) {
+  .then(i => {
     inst = i;
     log.log('info', 'Servidor iniciado com sucesso na porta %s e na interface' +
       ' %s', config.hapi.connection.port, config.hapi.connection.host);
   });
 
 // Vamos escutar se o sistema manda o servidor parar
-process.on(config.server.signal, function() {
+process.on(config.server.signal, () => {
   log.log('info', 'O servidor recebeu o sinal %s, iniciando parada',
     config.server.signal);
 
   // Enviamos a instância criada para o método stop
   server.stop(inst)
-    .then(function() {
+    .then(() => {
       log.log('Servidor parou, finalizando processo.');
       process.exit();
     });
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
-var fs = require('fs');
-module.exports = [];
+import fs from 'fs';
+
+let modules = [];
 
 fs.readdirSync(__dirname)
   .filter(function(ctrl) {
     return ctrl.match(/-ctrl\.js/i);
   })
   .forEach(function(ctrl) {
-    module.exports.push(require('./' + ctrl));
+    modules.push(require('./' + ctrl));
   });
+
+export default modules;
